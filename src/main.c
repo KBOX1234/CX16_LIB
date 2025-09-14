db6984716980e864f460f543c63b18b3bd34b04c
KBOX1234 <KBOXKBOX1234@gmail.com>
2025-09-14 18:24:00 +0200

    s

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdint.h>
 
-int main(){
-    printf("hello world\n");
+unsigned int larg;
+int i;
+
+int main() {
+
+    larg = 2;
+
+    for(i = 0; i < 15; i++){
+        i = i * 2;
+    }
+
+    printf("big num: %d\n", larg);
 
     return 0;
+
 }
